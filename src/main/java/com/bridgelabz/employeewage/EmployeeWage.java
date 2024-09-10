bd2bbff5845824c8beb9fde8c8e8387b8ea7c761
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-10 21:36:40 +0530

    Added: Calculate Employee wages

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -5,5 +5,19 @@ import org.w3c.dom.ls.LSOutput;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee problems");
+            int isFullTime = 1;
+            int empRatePerHour = 20;
+            int empHrs=0;
+            int empWage=0;
+            double empCheck = Math.floor(Math.random()*10)%2;
+            if(empCheck==isFullTime)
+            {
+                empHrs=8;
+            }
+            else{
+                empHrs=0;
+            }
+            empWage=empHrs*empRatePerHour;
+            System.out.println("Emp Wages : "+empWage);
     }
 }
