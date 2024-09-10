0682850780fbda9c9e37e69d760bf418df007df7
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-10 22:05:09 +0530

    Added: Add part time employee and find wages for them

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -5,15 +5,19 @@ import org.w3c.dom.ls.LSOutput;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee problems");
-            int isFullTime = 1;
+            int isFullTime = 2;
+            int isPartTime = 1;
             int empRatePerHour = 20;
             int empHrs=0;
             int empWage=0;
-            double empCheck = Math.floor(Math.random()*10)%2;
+            double empCheck = Math.floor(Math.random()*10)%3;
             if(empCheck==isFullTime)
             {
                 empHrs=8;
                 System.out.println("Employee is Present..");
+            }else if(empCheck==isPartTime)
+            {
+                empHrs=4;
             }
             else{
                 empHrs=0;
