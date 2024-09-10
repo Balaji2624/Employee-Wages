93b620448a45bca27b3a84a20e04dee2ee92b1db
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-10 22:52:36 +0530

    Added: Calculate total employee wage using while loop

--- src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -7,15 +7,18 @@ public class EmployeeWage {
     public static final int isFullTime =2;
     public static final int empRatePerHour=20;
     public static final int numOfWorking=2;
+    public static final int MaxHrsPerMonth=10;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee problems");
 
             int empHrs=0;
-            int empWage=0;
-            int totalEmpWage=0;
-            for(int day=0;day<numOfWorking;day++) {
+            int totalEmpHrs=0;
+            int totalWorkingDays=0;
+            while(totalEmpHrs <= MaxHrsPerMonth && totalWorkingDays < numOfWorking) {
+                totalWorkingDays++;
                 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-                switch (empCheck) {
+                 switch (empCheck) {
                     case isPartTime:
                         empHrs = 4;
                         break;
@@ -26,10 +29,10 @@ public class EmployeeWage {
                         empHrs = 0;
                 }
 
-                empWage = empHrs * empRatePerHour;
-                totalEmpWage = totalEmpWage + empWage;
-                System.out.println("Emp Wages : " + empWage);
+                totalEmpHrs=totalEmpHrs+empHrs;
+                System.out.println("day#: "+totalWorkingDays+" Emp Hrours : "+empHrs);
             }
+        int totalEmpWage=totalEmpHrs * empRatePerHour;
         System.out.println("Total Emp Wages : " + totalEmpWage);
     }
 }
