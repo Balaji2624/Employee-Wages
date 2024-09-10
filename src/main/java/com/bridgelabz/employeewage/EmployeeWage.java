757a7d05a61aec96c1523ad32b98adfb12dac6bd
Balaji2624 <balajisapkal3062001@gmail.com>
2024-09-10 21:28:24 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bridgelabz/employeewage/EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeewage;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee problems");
+    }
+}
